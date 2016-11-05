c6ecbc8369450800208d4543920c43b43a06533c
CpOuyang <savagebugle@gmail.com>
2016-11-06 05:28:58 +0800

    Create ElvUI.js

---
+++ ElvUI.js
@@ -0,0 +1,79 @@
+{
+    ["databars"] = {
+        ["artifact"] = {
+            ["height"] = 5,
+            ["orientation"] = "HORIZONTAL",
+            ["width"] = 576,
+        },
+        ["honor"] = {
+            ["enable"] = false,
+        },
+    },
+    ["currentTutorial"] = 6,
+    ["general"] = {
+        ["topPanel"] = false,
+        ["bottomPanel"] = false,
+        ["objectiveFrameHeight"] = 720,
+    },
+    ["movers"] = {
+        ["ElvUF_Raid40Mover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,432",
+        ["PetAB"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-118,61",
+        ["ElvUF_RaidMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,427",
+        ["TalkingHeadFrameMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,343",
+        ["BossButton"] = "BOTTOM,ElvUIParent,BOTTOM,0,226",
+        ["ElvAB_5"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-8,28",
+        ["ZoneAbility"] = "BOTTOM,ElvUIParent,BOTTOM,0,289",
+        ["ObjectiveFrameMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-205,-264",
+        ["ElvUF_RaidpetMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,736",
+        ["ShiftAB"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,1076",
+        ["ElvUF_TargetCastbarMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,191",
+        ["ElvUF_TargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,210",
+        ["MicrobarMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-144,28",
+        ["ElvUF_PlayerMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,210",
+        ["ElvAB_3"] = "BOTTOM,ElvUIParent,BOTTOM,0,51",
+        ["ElvUF_PartyMover"] = "BOTTOMLEFT,ElvUIParent,BOTTOMLEFT,4,195",
+        ["ArtifactBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,100",
+        ["ElvUF_PetMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,145",
+        ["ElvUF_TargetTargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,145",
+    },
+    ["actionbar"] = {
+        ["bar3"] = {
+            ["buttonspacing"] = 0,
+            ["buttonsPerRow"] = 12,
+            ["buttonsize"] = 48,
+            ["buttons"] = 12,
+        },
+        ["fontSize"] = 16,
+        ["bar1"] = {
+            ["buttonspacing"] = 0,
+            ["buttonsize"] = 48,
+        },
+        ["bar5"] = {
+            ["buttonsize"] = 31,
+            ["buttons"] = 4,
+        },
+        ["fontOutline"] = "NONE",
+        ["backdropSpacingConverted"] = true,
+        ["barPet"] = {
+            ["point"] = "TOPLEFT",
+            ["buttonsPerRow"] = 10,
+            ["buttonsize"] = 28,
+            ["backdrop"] = false,
+        },
+        ["microbar"] = {
+            ["enabled"] = true,
+        },
+        ["bar4"] = {
+            ["enabled"] = false,
+        },
+    },
+    ["unitframe"] = {
+        ["fontOutline"] = "NONE",
+        ["fontSize"] = 18,
+    },
+    ["chat"] = {
+        ["timeStampFormat"] = "%H:%M:%S ",
+        ["font"] = "聊天",
+        ["panelBackdrop"] = "LEFT",
+    },
+}::profile::Cipherarrow - 天空之牆
