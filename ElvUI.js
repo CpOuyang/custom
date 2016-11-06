eb3f112f5fc28b7258713bf4f729e389c801032a
CpOuyang <savagebugle@gmail.com>
2016-11-06 16:05:29 +0800

    Update ElvUI.js

--- ElvUI.js
+++ ElvUI.js
@@ -1,11 +1,6 @@
 {
     ["databars"] = {
         ["artifact"] = {
-            ["orientation"] = "HORIZONTAL",
-            ["height"] = 5,
-            ["width"] = 576,
-        },
-        ["experience"] = {
             ["height"] = 5,
             ["orientation"] = "HORIZONTAL",
             ["width"] = 576,
@@ -15,32 +10,43 @@
         },
     },
     ["currentTutorial"] = 7,
-    ["general"] = {
-        ["topPanel"] = false,
-        ["bottomPanel"] = false,
-        ["objectiveFrameHeight"] = 720,
+    ["sle"] = {
+        ["Armory"] = {
+            ["Character"] = {
+                ["Enable"] = false,
+            },
+            ["Inspect"] = {
+                ["Enable"] = false,
+            },
+        },
     },
     ["movers"] = {
         ["ElvUF_Raid40Mover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,432",
         ["PetAB"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-118,61",
         ["ElvUF_RaidMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,427",
+        ["LeftChatMover"] = "BOTTOMLEFT,ElvUIParent,BOTTOMLEFT,4,4",
         ["TalkingHeadFrameMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,343",
         ["BossButton"] = "BOTTOM,ElvUIParent,BOTTOM,0,226",
         ["ElvAB_5"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-8,28",
         ["ZoneAbility"] = "BOTTOM,ElvUIParent,BOTTOM,0,289",
-        ["ObjectiveFrameMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-205,-264",
+        ["ObjectiveFrameMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-65,-264",
         ["ElvUF_RaidpetMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,736",
         ["ShiftAB"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,1076",
-        ["ElvUF_TargetCastbarMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,191",
-        ["ElvUF_TargetTargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,145",
-        ["ArtifactBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,100",
+        ["ElvUF_TargetCastbarMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,221",
+        ["ElvUF_TargetTargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,180",
+        ["ElvUF_PetMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,180",
         ["MicrobarMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-144,28",
-        ["ElvUF_PlayerMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,210",
-        ["ElvUF_PetMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,145",
+        ["ElvUF_PlayerMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,240",
         ["ElvUF_PartyMover"] = "BOTTOMLEFT,ElvUIParent,BOTTOMLEFT,4,195",
-        ["ExperienceBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,104",
+        ["RightChatMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-4,4",
+        ["ArtifactBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,100",
         ["ElvAB_3"] = "BOTTOM,ElvUIParent,BOTTOM,0,51",
-        ["ElvUF_TargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,210",
+        ["ElvUF_TargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,240",
+    },
+    ["general"] = {
+        ["topPanel"] = false,
+        ["bottomPanel"] = false,
+        ["objectiveFrameHeight"] = 720,
     },
     ["actionbar"] = {
         ["bar3"] = {
@@ -59,7 +65,9 @@
             ["buttons"] = 4,
         },
         ["fontOutline"] = "NONE",
-        ["backdropSpacingConverted"] = true,
+        ["bar4"] = {
+            ["enabled"] = false,
+        },
         ["barPet"] = {
             ["point"] = "TOPLEFT",
             ["buttonsPerRow"] = 10,
@@ -69,9 +77,7 @@
         ["microbar"] = {
             ["enabled"] = true,
         },
-        ["bar4"] = {
-            ["enabled"] = false,
-        },
+        ["backdropSpacingConverted"] = true,
     },
     ["unitframe"] = {
         ["fontOutline"] = "NONE",
@@ -82,4 +88,4 @@
         ["font"] = "聊天",
         ["panelBackdrop"] = "LEFT",
     },
-}::profile::Cipherhorn - 天空之牆
+}::profile::Cipherarrow - 天空之牆
