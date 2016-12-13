407bb4d065c2bec6d21ee30a5c0e79bfbcb78ed2
CpOuyang <savagebugle@gmail.com>
2016-12-14 02:53:42 +0800

    Update ElvUI.js

--- ElvUI.js
+++ ElvUI.js
@@ -1,8 +1,8 @@
 {
     ["databars"] = {
         ["artifact"] = {
-            ["height"] = 5,
             ["orientation"] = "HORIZONTAL",
+            ["height"] = 5,
             ["width"] = 576,
         },
         ["experience"] = {
@@ -14,14 +14,15 @@
             ["enable"] = false,
         },
     },
-    ["currentTutorial"] = 4,
-    ["general"] = {
-        ["topPanel"] = false,
-        ["objectiveFrameHeight"] = 720,
-        ["bottomPanel"] = false,
-        ["minimap"] = {
-            ["locationText"] = "SHOW",
-            ["locationFontSize"] = 14,
+    ["currentTutorial"] = 9,
+    ["sle"] = {
+        ["Armory"] = {
+            ["Character"] = {
+                ["Enable"] = false,
+            },
+            ["Inspect"] = {
+                ["Enable"] = false,
+            },
         },
     },
     ["movers"] = {
@@ -40,65 +41,6 @@
         ["ArtifactBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,100",
         ["ElvUF_TargetCastbarMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,221",
         ["ElvAB_2"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-464,4",
-        ["ElvUF_PartyMover"] = "BOTTOMLEFT,ElvUIParent,BOTTOMLEFT,4,195",
-        ["MicrobarMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-144,28",
-        ["ElvUF_PetMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,180",
-        ["ElvUF_PlayerMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,240",
-        ["RightChatMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-4,4",
-        ["ExperienceBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,106",
-        ["ElvAB_3"] = "BOTTOM,ElvUIParent,BOTTOM,0,51",
-        ["ElvUF_TargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,240",
-    },
-    ["sle"] = {
-        ["Armory"] = {
-            ["Character"] = {
-                ["Enable"] = false,
-            },
-            ["Inspect"] = {
-                ["Enable"] = false,
-            },
-{
-    ["databars"] = {
-        ["artifact"] = {
-            ["orientation"] = "HORIZONTAL",
-            ["height"] = 5,
-            ["width"] = 576,
-        },
-        ["experience"] = {
-            ["orientation"] = "HORIZONTAL",
-            ["height"] = 5,
-            ["width"] = 576,
-        },
-        ["honor"] = {
-            ["enable"] = false,
-        },
-    },
-    ["currentTutorial"] = 12,
-    ["general"] = {
-        ["topPanel"] = false,
-        ["objectiveFrameHeight"] = 720,
-        ["bottomPanel"] = false,
-        ["minimap"] = {
-            ["locationText"] = "SHOW",
-            ["locationFontSize"] = 14,
-        },
-    },
-    ["movers"] = {
-        ["ElvUF_Raid40Mover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,477",
-        ["PetAB"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-118,61",
-        ["ElvUF_RaidMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,487",
-        ["ElvAB_2"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-464,4",
-        ["TalkingHeadFrameMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,343",
-        ["BossButton"] = "BOTTOM,ElvUIParent,BOTTOM,0,226",
-        ["ElvAB_5"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-8,28",
-        ["ZoneAbility"] = "BOTTOM,ElvUIParent,BOTTOM,0,289",
-        ["ObjectiveFrameMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-65,-264",
-        ["ElvUF_RaidpetMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,736",
-        ["ShiftAB"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,1076",
-        ["ElvUF_TargetTargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,180",
-        ["ArtifactBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,100",
-        ["ElvUF_TargetCastbarMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,221",
-        ["LeftChatMover"] = "BOTTOMLEFT,ElvUIParent,BOTTOMLEFT,4,4",
         ["ElvUF_PartyMover"] = "BOTTOMLEFT,ElvUIParent,BOTTOMLEFT,4,195",
         ["MicrobarMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-144,28",
         ["ElvUF_PetMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,180",
@@ -140,9 +82,7 @@
             ["buttonsize"] = 31,
             ["buttons"] = 4,
         },
-        ["bar4"] = {
-            ["enabled"] = false,
-        },
+        ["backdropSpacingConverted"] = true,
         ["fontOutline"] = "NONE",
         ["macrotext"] = true,
         ["barPet"] = {
@@ -154,14 +94,19 @@
         ["microbar"] = {
             ["enabled"] = true,
         },
-        ["backdropSpacingConverted"] = true,
+        ["bar4"] = {
+            ["enabled"] = false,
+        },
     },
     ["nameplates"] = {
-        ["fontSize"] = 12,
+        ["fontSize"] = 14,
+        ["fontOutline"] = "NONE",
+        ["loadDistance"] = 50,
+        ["clickableHeight"] = 15,
         ["units"] = {
             ["ENEMY_NPC"] = {
                 ["healthbar"] = {
-                    ["height"] = 16,
+                    ["height"] = 14,
                     ["text"] = {
                         ["enable"] = true,
                     },
@@ -169,27 +114,18 @@
             },
             ["FRIENDLY_NPC"] = {
                 ["healthbar"] = {
-                    ["enable"] = true,
-                    ["text"] = {
-                        ["enable"] = true,
-                    },
+                    ["height"] = 14,
                 },
             },
         },
-        ["fontOutline"] = "NONE",
-        ["loadDistance"] = 60,
-        ["clickableHeight"] = 30,
-        ["showFriendlyCombat"] = "TOGGLE_ON",
-        ["showEnemyCombat"] = "TOGGLE_ON",
     },
-    ["sle"] = {
-        ["Armory"] = {
-            ["Character"] = {
-                ["Enable"] = false,
-            },
-            ["Inspect"] = {
-                ["Enable"] = false,
-            },
+    ["general"] = {
+        ["topPanel"] = false,
+        ["objectiveFrameHeight"] = 720,
+        ["bottomPanel"] = false,
+        ["minimap"] = {
+            ["locationText"] = "SHOW",
+            ["locationFontSize"] = 14,
         },
     },
-}::profile::Cipherarrow - 天空之牆
+}::profile::Cipherarrow
