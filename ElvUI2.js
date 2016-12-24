81b2319b6a8903a93ac9f7b9954ea59b05011b59
CpOuyang <savagebugle@gmail.com>
2016-12-24 21:27:16 +0800

    Create ElvUI2.js

---
+++ ElvUI2.js
@@ -0,0 +1,148 @@
+{
+    ["databars"] = {
+        ["artifact"] = {
+            ["orientation"] = "HORIZONTAL",
+            ["textFormat"] = "CURREM",
+            ["height"] = 10,
+            ["width"] = 270,
+        },
+        ["experience"] = {
+            ["height"] = 10,
+            ["orientation"] = "HORIZONTAL",
+            ["width"] = 270,
+        },
+        ["honor"] = {
+            ["enable"] = false,
+        },
+    },
+    ["currentTutorial"] = 8,
+    ["general"] = {
+        ["topPanel"] = false,
+        ["objectiveFrameHeight"] = 720,
+        ["bottomPanel"] = false,
+        ["minimap"] = {
+            ["locationText"] = "SHOW",
+            ["locationFontSize"] = 14,
+        },
+    },
+    ["movers"] = {
+        ["ElvUF_Raid40Mover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,742",
+        ["PetAB"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-118,61",
+        ["ElvAB_1"] = "BOTTOM,ElvUIParent,BOTTOM,0,6",
+        ["ElvAB_2"] = "BOTTOM,ElvUIParent,BOTTOM,0,106",
+        ["TalkingHeadFrameMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,343",
+        ["BossButton"] = "BOTTOM,ElvUIParent,BOTTOM,0,226",
+        ["ElvAB_5"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-8,28",
+        ["ZoneAbility"] = "BOTTOM,ElvUIParent,BOTTOM,0,289",
+        ["ElvUF_TargetTargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,210",
+        ["ArtifactBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,333",
+        ["ObjectiveFrameMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-65,-264",
+        ["ElvUF_RaidpetMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,736",
+        ["ShiftAB"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,1076",
+        ["ElvUF_RaidMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,742",
+        ["ElvUF_PartyMover"] = "BOTTOMLEFT,ElvUIParent,BOTTOMLEFT,4,460",
+        ["ElvUF_TargetCastbarMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,251",
+        ["ElvUI_RMBarMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-369,381",
+        ["ElvUF_PlayerMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,270",
+        ["MicrobarMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-144,28",
+        ["ElvUF_PetMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,210",
+        ["LeftChatMover"] = "BOTTOMLEFT,ElvUIParent,BOTTOMLEFT,4,4",
+        ["RightChatMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-4,4",
+        ["ExperienceBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,342",
+        ["ElvAB_3"] = "BOTTOM,ElvUIParent,BOTTOM,0,56",
+        ["ElvUF_TargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,270",
+    },
+    ["chat"] = {
+        ["font"] = "聊天",
+        ["timeStampFormat"] = "%H:%M:%S ",
+        ["panelBackdrop"] = "LEFT",
+        ["tabFontSize"] = 11,
+        ["panelTabBackdrop"] = true,
+    },
+    ["unitframe"] = {
+        ["fontSize"] = 18,
+        ["units"] = {
+            ["target"] = {
+                ["height"] = 64,
+            },
+            ["player"] = {
+                ["height"] = 64,
+            },
+        },
+        ["fontOutline"] = "NONE",
+    },
+    ["actionbar"] = {
+        ["bar3"] = {
+            ["buttonspacing"] = 10,
+            ["buttonsPerRow"] = 12,
+            ["buttonsize"] = 40,
+            ["buttons"] = 12,
+        },
+        ["fontSize"] = 16,
+        ["bar2"] = {
+            ["enabled"] = true,
+            ["buttons"] = 8,
+            ["buttonspacing"] = 10,
+            ["buttonsize"] = 40,
+            ["showGrid"] = false,
+        },
+        ["bar1"] = {
+            ["buttonspacing"] = 10,
+            ["buttonsize"] = 40,
+        },
+        ["bar5"] = {
+            ["buttonsize"] = 31,
+            ["buttons"] = 4,
+        },
+        ["bar4"] = {
+            ["enabled"] = false,
+        },
+        ["barPet"] = {
+            ["point"] = "TOPLEFT",
+            ["buttonsPerRow"] = 10,
+            ["buttonsize"] = 28,
+            ["backdrop"] = false,
+        },
+        ["fontOutline"] = "NONE",
+        ["macrotext"] = true,
+        ["backdropSpacingConverted"] = true,
+        ["microbar"] = {
+            ["enabled"] = true,
+        },
+        ["raidmarkersbar"] = {
+            ["scale"] = 1.2,
+            ["visible"] = "show",
+        },
+    },
+    ["nameplates"] = {
+        ["fontSize"] = 14,
+        ["fontOutline"] = "NONE",
+        ["loadDistance"] = 50,
+        ["clickableHeight"] = 15,
+        ["units"] = {
+            ["ENEMY_NPC"] = {
+                ["healthbar"] = {
+                    ["height"] = 14,
+                    ["text"] = {
+                        ["enable"] = true,
+                    },
+                },
+            },
+            ["FRIENDLY_NPC"] = {
+                ["healthbar"] = {
+                    ["height"] = 14,
+                },
+            },
+        },
+    },
+    ["sle"] = {
+        ["Armory"] = {
+            ["Character"] = {
+                ["Enable"] = false,
+            },
+            ["Inspect"] = {
+                ["Enable"] = false,
+            },
+        },
+    },
+}::profile::Cipherhammer
