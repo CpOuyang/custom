f9d9e587ebbd7866c26eb01afab52e37e35c2ec4
CpOuyang <savagebugle@gmail.com>
2017-01-16 00:23:36 +0800

    Update ElvUI2.js

--- ElvUI2.js
+++ ElvUI2.js
@@ -1,75 +1,71 @@
 {
     ["databars"] = {
         ["artifact"] = {
-            ["orientation"] = "HORIZONTAL",
-            ["textFormat"] = "CURREM",
             ["height"] = 10,
+            ["textFormat"] = "CURREM",
+            ["orientation"] = "HORIZONTAL",
             ["width"] = 270,
         },
         ["experience"] = {
-            ["height"] = 10,
+            ["height"] = 5,
             ["orientation"] = "HORIZONTAL",
-            ["width"] = 270,
+            ["width"] = 576,
         },
         ["honor"] = {
             ["enable"] = false,
         },
     },
-    ["currentTutorial"] = 5,
-    ["sle"] = {
-        ["Armory"] = {
-            ["Inspect"] = {
-                ["Enable"] = false,
-            },
-            ["Character"] = {
-                ["Enable"] = false,
-            },
+    ["currentTutorial"] = 9,
+    ["general"] = {
+        ["topPanel"] = false,
+        ["objectiveFrameHeight"] = 720,
+        ["bottomPanel"] = false,
+        ["minimap"] = {
+            ["locationText"] = "SHOW",
+            ["locationFontSize"] = 14,
         },
     },
     ["movers"] = {
         ["ElvUF_Raid40Mover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,742",
         ["PetAB"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-118,61",
-        ["ElvUF_RaidMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,742",
+        ["ElvAB_1"] = "BOTTOM,ElvUIParent,BOTTOM,0,6",
         ["ElvAB_2"] = "BOTTOM,ElvUIParent,BOTTOM,0,106",
         ["TalkingHeadFrameMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,343",
         ["BossButton"] = "BOTTOM,ElvUIParent,BOTTOM,0,226",
         ["ElvAB_5"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-8,28",
         ["ZoneAbility"] = "BOTTOM,ElvUIParent,BOTTOM,0,289",
-        ["ElvUF_TargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,270",
-        ["ElvAB_3"] = "BOTTOM,ElvUIParent,BOTTOM,0,56",
+        ["ElvUF_TargetTargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,210",
+        ["ArtifactBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,333",
         ["ObjectiveFrameMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-65,-264",
         ["ElvUF_RaidpetMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,736",
         ["ShiftAB"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,1076",
-        ["ElvAB_1"] = "BOTTOM,ElvUIParent,BOTTOM,0,6",
-        ["RightChatMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-4,4",
+        ["ElvUF_RaidMover"] = "TOPLEFT,ElvUIParent,BOTTOMLEFT,4,742",
+        ["ElvUF_PartyMover"] = "BOTTOMLEFT,ElvUIParent,BOTTOMLEFT,4,460",
         ["ElvUF_TargetCastbarMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,251",
-        ["LeftChatMover"] = "BOTTOMLEFT,ElvUIParent,BOTTOMLEFT,4,4",
-        ["ElvUF_PetMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,210",
-        ["MicrobarMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-144,28",
         ["ElvUF_PlayerMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,270",
         ["ElvUI_RMBarMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-369,381",
-        ["ElvUF_PartyMover"] = "BOTTOMLEFT,ElvUIParent,BOTTOMLEFT,4,460",
-        ["ExperienceBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,342",
-        ["ArtifactBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,333",
-        ["ElvUF_TargetTargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,210",
+        ["MicrobarMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-144,28",
+        ["ElvUF_PetMover"] = "BOTTOM,ElvUIParent,BOTTOM,-200,210",
+        ["LeftChatMover"] = "BOTTOMLEFT,ElvUIParent,BOTTOMLEFT,4,4",
+        ["RightChatMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-4,4",
+        ["ExperienceBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,0,106",
+        ["ElvAB_3"] = "BOTTOM,ElvUIParent,BOTTOM,0,56",
+        ["ElvUF_TargetMover"] = "BOTTOM,ElvUIParent,BOTTOM,200,270",
     },
     ["bags"] = {
-        ["countFontSize"] = 12,
-        ["itemLevelFontSize"] = 12,
-        ["itemLevelFontOutline"] = "OUTLINE",
         ["ignoredItems"] = {
-            ["tt:任務"] = "tt:任務",
-            ["tt:斯卡格"] = "tt:斯卡格",
-            ["tt:唯一"] = "tt:唯一",
-            ["tt:神兵之力"] = "tt:神兵之力",
+            ["n:鬼燈果|n:狐花|n:紫地根草|n:幻夢草"] = "n:鬼燈果|n:狐花|n:紫地根草|n:幻夢草",
+            ["tt:星輝玫瑰"] = "tt:星輝玫瑰",
+            ["tt:爐石"] = "tt:爐石",
+            ["n:伊瑟拉種子"] = "n:伊瑟拉種子",
         },
         ["countFontOutline"] = "OUTLINE",
+        ["itemLevelFontOutline"] = "OUTLINE",
     },
     ["chat"] = {
-        ["font"] = "聊天",
         ["timeStampFormat"] = "%H:%M:%S ",
+        ["font"] = "聊天",
         ["panelBackdrop"] = "LEFT",
-        ["tabFontSize"] = 11,
         ["panelTabBackdrop"] = true,
     },
     ["unitframe"] = {
@@ -78,11 +74,22 @@
             ["target"] = {
                 ["height"] = 64,
             },
+            ["raid"] = {
+                ["height"] = 60,
+                ["width"] = 90,
+            },
+            ["raid40"] = {
+                ["height"] = 60,
+                ["width"] = 90,
+            },
             ["player"] = {
                 ["height"] = 64,
             },
         },
         ["fontOutline"] = "NONE",
+    },
+    ["datatexts"] = {
+        ["fontSize"] = 14,
     },
     ["actionbar"] = {
         ["bar3"] = {
@@ -107,7 +114,9 @@
             ["buttonsize"] = 31,
             ["buttons"] = 4,
         },
-        ["backdropSpacingConverted"] = true,
+        ["bar4"] = {
+            ["enabled"] = false,
+        },
         ["barPet"] = {
             ["point"] = "TOPLEFT",
             ["buttonsPerRow"] = 10,
@@ -116,15 +125,13 @@
         },
         ["fontOutline"] = "NONE",
         ["macrotext"] = true,
-        ["bar4"] = {
-            ["enabled"] = false,
-        },
+        ["backdropSpacingConverted"] = true,
         ["microbar"] = {
             ["enabled"] = true,
         },
         ["raidmarkersbar"] = {
-            ["visible"] = "show",
             ["scale"] = 1.2,
+            ["visible"] = "show",
         },
     },
     ["nameplates"] = {
@@ -148,13 +155,14 @@
             },
         },
     },
-    ["general"] = {
-        ["topPanel"] = false,
-        ["objectiveFrameHeight"] = 720,
-        ["bottomPanel"] = false,
-        ["minimap"] = {
-            ["locationText"] = "SHOW",
-            ["locationFontSize"] = 14,
+    ["sle"] = {
+        ["Armory"] = {
+            ["Character"] = {
+                ["Enable"] = false,
+            },
+            ["Inspect"] = {
+                ["Enable"] = false,
+            },
         },
     },
-}::profile::Cipherdin
+}::profile::Cipherarrow
