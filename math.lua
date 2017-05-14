caf86b6ed592579404d61a2caedc581be1f9dc9c
CpOuyang <savagebugle@gmail.com>
2017-05-14 20:18:37 +0800

    Create math.lua

---
+++ math.lua
@@ -0,0 +1,33 @@
+function E:ShortValue(v)
+	if E.db.general.numberPrefixStyle == "METRIC" then
+		if abs(v) >= 1e9 then
+			return format("%.1fG", v / 1e9)
+		elseif abs(v) >= 1e6 then
+			return format("%.1fM", v / 1e6)
+		elseif abs(v) >= 1e3 then
+			return format("%.1fk", v / 1e3)
+		else
+			return format("%d", v)
+		end
+	elseif E.db.general.numberPrefixStyle == "CHINESE" then
+		if abs(v) >= 1e8 then
+--			return format("%.1fY", v / 1e8)
+			return format("%.2f億", v / 1e8)
+		elseif abs(v) >= 1e4 then
+--			return format("%.1fW", v / 1e4)
+			return format("%.0f萬", v / 1e4)
+		else
+			return format("%d", v)
+		end
+	else
+		if abs(v) >= 1e9 then
+			return format("%.1fB", v / 1e9)
+		elseif abs(v) >= 1e6 then
+			return format("%.1fM", v / 1e6)
+		elseif abs(v) >= 1e3 then
+			return format("%.1fK", v / 1e3)
+		else
+			return format("%d", v)
+		end
+	end
+end
