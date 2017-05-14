381f8532c187009057c4350ef3718a3b90c6fd4b
CpOuyang <savagebugle@gmail.com>
2017-05-14 20:13:56 +0800

    Update Skada.lua

--- Skada.lua
+++ Skada.lua
@@ -2,9 +2,9 @@ function Skada:FormatNumber(number)
 	if number then
 		if self.db.profile.numberformat == 1 then
 			if number > 100000000 then
-				return ("%02.1f億"):format(number / 100000000)
+				return ("%02.2f億"):format(number / 100000000)
 			elseif number > 10000 then
-				return ("%02.1f萬"):format(number / 10000)
+				return ("%02.0f萬"):format(number / 10000)
 			else
 				return ("%02.1f千"):format(number / 1000)
 			end
