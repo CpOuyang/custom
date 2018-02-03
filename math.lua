978bce8d8e0204eae9d39657719c4154302e3036
CpOuyang <savagebugle@gmail.com>
2018-02-03 11:04:43 +0800

    Update math.lua

--- math.lua
+++ math.lua
@@ -9,59 +9,56 @@ function comma_value(amount)
 	return formatted
 end
 
+local shortValueDec
 function E:ShortValue(v)
+	shortValueDec = format("%%.%df", E.db.general.decimalLength or 1)
 	if E.db.general.numberPrefixStyle == "METRIC" then
 		if abs(v) >= 1e9 then
-			return format("%.1fG", v / 1e9)
+			return format(shortValueDec.."G", v / 1e9)
 		elseif abs(v) >= 1e6 then
-			return format("%.1fM", v / 1e6)
+			return format(shortValueDec.."M", v / 1e6)
 		elseif abs(v) >= 1e3 then
-			return format("%.1fk", v / 1e3)
+			return format(shortValueDec.."k", v / 1e3)
 		else
-			return format("%d", v)
+			return format("%s", v)
 		end
 	elseif E.db.general.numberPrefixStyle == "CHINESE" then
-		if abs(v) >= 10e8 then
-			return format("%.1f億", v / 1e8)
-		elseif abs(v) >= 1e8 then
-			return format("%.2f億", v / 1e8)
-		elseif abs(v) >= 10e4 then
-			return format("%.0f萬", v / 1e4)
+		if abs(v) >= 1e8 then
+			return format(shortValueDec.."億", v / 1e8)
 		elseif abs(v) >= 1e4 then
-			return format("%.1f萬", v / 1e4)
+			return format(shortValueDec.."萬", v / 1e4)
 		else
-			return format("%d", v)
+			return format("%s", v)
 		end
---		return comma_value(v)
 	elseif E.db.general.numberPrefixStyle == "KOREAN" then
 		if abs(v) >= 1e8 then
-			return format("%.1f억", v / 1e8)
+			return format(shortValueDec.."억", v / 1e8)
 		elseif abs(v) >= 1e4 then
-			return format("%.1f만", v / 1e4)
+			return format(shortValueDec.."만", v / 1e4)
 		elseif abs(v) >= 1e3 then
-			return format("%.1f천", v / 1e3)
+			return format(shortValueDec.."천", v / 1e3)
 		else
-			return format("%d", v)
+			return format("%s", v)
 		end
 	elseif E.db.general.numberPrefixStyle == "GERMAN" then
 		if abs(v) >= 1e9 then
-			return format("%.1fMrd", v / 1e9)
+			return format(shortValueDec.."Mrd", v / 1e9)
 		elseif abs(v) >= 1e6 then
-			return format("%.1fMio", v / 1e6)
+			return format(shortValueDec.."Mio", v / 1e6)
 		elseif abs(v) >= 1e3 then
-			return format("%.1fTsd", v / 1e3)
+			return format(shortValueDec.."Tsd", v / 1e3)
 		else
-			return format("%d", v)
+			return format("%s", v)
 		end
 	else
 		if abs(v) >= 1e9 then
-			return format("%.1fB", v / 1e9)
+			return format(shortValueDec.."B", v / 1e9)
 		elseif abs(v) >= 1e6 then
-			return format("%.1fM", v / 1e6)
+			return format(shortValueDec.."M", v / 1e6)
 		elseif abs(v) >= 1e3 then
-			return format("%.1fK", v / 1e3)
+			return format(shortValueDec.."K", v / 1e3)
 		else
-			return format("%d", v)
+			return format("%s", v)
 		end
 	end
 end
