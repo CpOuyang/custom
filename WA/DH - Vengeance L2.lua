41911cc09245d98f024517d58b4543e2b057fe45
CpOuyang <savagebugle@gmail.com>
2019-03-10 22:06:48 +0800

    Create DH - Vengeance L2.lua

---
+++ WA/DH - Vengeance L2.lua
@@ -0,0 +1,101 @@
+{
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["controlledChildren"] = {
+        [1] = "折磨",
+        [2] = "投擲戰刃",
+        [3] = "干擾",
+        [4] = "靈魂屏障",
+        [5] = "靈魂炸彈",
+    },
+    ["borderBackdrop"] = "Blizzard Tooltip",
+    ["authorOptions"] = {
+    },
+    ["border"] = false,
+    ["borderEdge"] = "None",
+    ["regionType"] = "group",
+    ["borderSize"] = 16,
+    ["uid"] = "bJBypWGNn3Z",
+    ["borderColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["animation"] = {
+        ["start"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+        ["main"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+        ["finish"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+    },
+    ["expanded"] = true,
+    ["triggers"] = {
+        [1] = {
+            ["trigger"] = {
+                ["debuffType"] = "HELPFUL",
+                ["type"] = "aura2",
+                ["spellIds"] = {
+                },
+                ["subeventSuffix"] = "_CAST_START",
+                ["unit"] = "player",
+                ["subeventPrefix"] = "SPELL",
+                ["event"] = "Health",
+                ["names"] = {
+                },
+            },
+            ["untrigger"] = {
+            },
+        },
+    },
+    ["xOffset"] = -100,
+    ["internalVersion"] = 11,
+    ["yOffset"] = -160,
+    ["selfPoint"] = "BOTTOMLEFT",
+    ["id"] = "_DH - Vengeance L2",
+    ["scale"] = 1,
+    ["frameStrata"] = 1,
+    ["anchorFrameType"] = "SCREEN",
+    ["anchorPoint"] = "CENTER",
+    ["borderInset"] = 11,
+    ["config"] = {
+    },
+    ["borderOffset"] = 5,
+    ["conditions"] = {
+    },
+    ["load"] = {
+        ["use_class"] = "true",
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["single"] = "DEMONHUNTER",
+            ["multi"] = {
+            },
+        },
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["finish"] = {
+        },
+        ["init"] = {
+        },
+    },
+}
