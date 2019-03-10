4bd7b64426ed131b462b65d326c164964342a651
CpOuyang <savagebugle@gmail.com>
2019-03-11 00:23:56 +0800

    Update DH - Vengeance L1.lua

--- WA/DH - Vengeance L1.lua
+++ WA/DH - Vengeance L1.lua
@@ -19,7 +19,7 @@
     ["borderEdge"] = "None",
     ["regionType"] = "group",
     ["borderSize"] = 16,
-    ["uid"] = "BGH4t(I(7H)",
+    ["xOffset"] = -100,
     ["borderColor"] = {
         [1] = 1,
         [2] = 1,
@@ -40,7 +40,14 @@
             ["type"] = "none",
         },
     },
-    ["expanded"] = true,
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["finish"] = {
+        },
+        ["init"] = {
+        },
+    },
     ["triggers"] = {
         [1] = {
             ["trigger"] = {
@@ -59,27 +66,29 @@
             },
         },
     },
-    ["xOffset"] = -100,
+    ["expanded"] = false,
     ["internalVersion"] = 11,
     ["yOffset"] = -120,
     ["selfPoint"] = "BOTTOMLEFT",
     ["id"] = "_DH - Vengeance L1",
-    ["scale"] = 1,
+    ["anchorPoint"] = "CENTER",
     ["frameStrata"] = 1,
     ["anchorFrameType"] = "SCREEN",
-    ["anchorPoint"] = "CENTER",
-    ["borderInset"] = 11,
     ["config"] = {
     },
+    ["uid"] = "BGH4t(I(7H)",
+    ["borderInset"] = 11,
     ["borderOffset"] = 5,
     ["conditions"] = {
     },
     ["load"] = {
+        ["use_class"] = "true",
         ["spec"] = {
             ["multi"] = {
             },
         },
         ["class"] = {
+            ["single"] = "DEMONHUNTER",
             ["multi"] = {
             },
         },
@@ -88,12 +97,5 @@
             },
         },
     },
-    ["actions"] = {
-        ["start"] = {
-        },
-        ["finish"] = {
-        },
-        ["init"] = {
-        },
-    },
+    ["scale"] = 1,
 }
