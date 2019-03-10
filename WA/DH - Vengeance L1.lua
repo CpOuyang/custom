8f077cfe0cfa2a187cde42df2eb4b9d888888e9f
CpOuyang <savagebugle@gmail.com>
2019-03-10 21:55:17 +0800

    Create DH - Vengeance L1.lua

---
+++ WA/DH - Vengeance L1.lua
@@ -0,0 +1,99 @@
+{
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["controlledChildren"] = {
+        [1] = "火焰符印",
+        [2] = "奧流之術",
+        [3] = "煉獄轟擊",
+        [4] = "惡魔化身",
+        [5] = "吞噬魔法",
+    },
+    ["borderBackdrop"] = "Blizzard Tooltip",
+    ["authorOptions"] = {
+    },
+    ["border"] = false,
+    ["borderEdge"] = "None",
+    ["regionType"] = "group",
+    ["borderSize"] = 16,
+    ["uid"] = "BGH4t(I(7H)",
+    ["borderColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["animation"] = {
+        ["start"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+        ["main"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+        ["finish"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+    },
+    ["expanded"] = true,
+    ["triggers"] = {
+        [1] = {
+            ["trigger"] = {
+                ["debuffType"] = "HELPFUL",
+                ["type"] = "aura2",
+                ["spellIds"] = {
+                },
+                ["subeventSuffix"] = "_CAST_START",
+                ["unit"] = "player",
+                ["subeventPrefix"] = "SPELL",
+                ["event"] = "Health",
+                ["names"] = {
+                },
+            },
+            ["untrigger"] = {
+            },
+        },
+    },
+    ["xOffset"] = -100,
+    ["internalVersion"] = 11,
+    ["yOffset"] = -120,
+    ["selfPoint"] = "BOTTOMLEFT",
+    ["id"] = "_DH - Vengence L1",
+    ["scale"] = 1,
+    ["frameStrata"] = 1,
+    ["anchorFrameType"] = "SCREEN",
+    ["anchorPoint"] = "CENTER",
+    ["borderInset"] = 11,
+    ["config"] = {
+    },
+    ["borderOffset"] = 5,
+    ["conditions"] = {
+    },
+    ["load"] = {
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["multi"] = {
+            },
+        },
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["finish"] = {
+        },
+        ["init"] = {
+        },
+    },
+}
