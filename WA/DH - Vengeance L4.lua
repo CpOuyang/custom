096a0df74bfc4214b4ecd10b3520b8e4e37ec157
CpOuyang <savagebugle@gmail.com>
2019-03-10 22:07:53 +0800

    Create DH - Vengeance L4.lua

---
+++ WA/DH - Vengeance L4.lua
@@ -0,0 +1,97 @@
+{
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["controlledChildren"] = {
+        [1] = "囚禁",
+    },
+    ["borderBackdrop"] = "Blizzard Tooltip",
+    ["authorOptions"] = {
+    },
+    ["border"] = false,
+    ["borderEdge"] = "None",
+    ["regionType"] = "group",
+    ["borderSize"] = 16,
+    ["expanded"] = true,
+    ["borderColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["selfPoint"] = "BOTTOMLEFT",
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["init"] = {
+        },
+        ["finish"] = {
+        },
+    },
+    ["triggers"] = {
+        [1] = {
+            ["trigger"] = {
+                ["names"] = {
+                },
+                ["type"] = "aura2",
+                ["spellIds"] = {
+                },
+                ["subeventSuffix"] = "_CAST_START",
+                ["unit"] = "player",
+                ["subeventPrefix"] = "SPELL",
+                ["event"] = "Health",
+                ["debuffType"] = "HELPFUL",
+            },
+            ["untrigger"] = {
+            },
+        },
+    },
+    ["xOffset"] = 0,
+    ["internalVersion"] = 11,
+    ["yOffset"] = -240,
+    ["animation"] = {
+        ["start"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["main"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["finish"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+    },
+    ["id"] = "_DH - Vengeance L4",
+    ["borderOffset"] = 5,
+    ["frameStrata"] = 1,
+    ["anchorFrameType"] = "SCREEN",
+    ["borderInset"] = 11,
+    ["config"] = {
+    },
+    ["anchorPoint"] = "CENTER",
+    ["scale"] = 1,
+    ["conditions"] = {
+    },
+    ["load"] = {
+        ["use_class"] = "true",
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["single"] = "DEMONHUNTER",
+            ["multi"] = {
+            },
+        },
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["uid"] = "uvCWd9oe8sf",
+}
