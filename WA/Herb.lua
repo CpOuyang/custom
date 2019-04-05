153354fda9967f230e25f828cc82a82b0bed9a4f
CpOuyang <savagebugle@gmail.com>
2019-04-05 23:27:30 +0800

    Create Herb.lua

---
+++ WA/Herb.lua
@@ -0,0 +1,102 @@
+{
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["controlledChildren"] = {
+        [1] = "海莖草",
+        [2] = "亞昆達之噬",
+        [3] = "河芽草",
+        [4] = "海妖花粉",
+        [5] = "星苔",
+        [6] = "冬吻花",
+        [7] = "錨草",
+    },
+    ["borderBackdrop"] = "Blizzard Tooltip",
+    ["authorOptions"] = {
+    },
+    ["border"] = false,
+    ["borderEdge"] = "None",
+    ["regionType"] = "group",
+    ["borderSize"] = 16,
+    ["xOffset"] = 420,
+    ["borderColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["animation"] = {
+        ["start"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+        ["main"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+        ["finish"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+    },
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["finish"] = {
+        },
+        ["init"] = {
+        },
+    },
+    ["triggers"] = {
+        [1] = {
+            ["trigger"] = {
+                ["type"] = "aura2",
+                ["subeventSuffix"] = "_CAST_START",
+                ["event"] = "Health",
+                ["names"] = {
+                },
+                ["spellIds"] = {
+                },
+                ["subeventPrefix"] = "SPELL",
+                ["unit"] = "player",
+                ["debuffType"] = "HELPFUL",
+            },
+            ["untrigger"] = {
+            },
+        },
+    },
+    ["expanded"] = true,
+    ["internalVersion"] = 11,
+    ["yOffset"] = -455,
+    ["selfPoint"] = "BOTTOMLEFT",
+    ["id"] = "Herb",
+    ["anchorPoint"] = "CENTER",
+    ["frameStrata"] = 1,
+    ["anchorFrameType"] = "SCREEN",
+    ["borderInset"] = 11,
+    ["config"] = {
+    },
+    ["uid"] = "2WJJwdW7ciF",
+    ["borderOffset"] = 5,
+    ["conditions"] = {
+    },
+    ["load"] = {
+        ["use_class"] = false,
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["multi"] = {
+            },
+        },
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["scale"] = 1,
+}
